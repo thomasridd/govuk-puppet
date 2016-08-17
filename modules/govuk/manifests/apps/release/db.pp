c98f443a7a40c1d4f947ae9217735e84966d9bd5
Mikael <mikellallison@gmail.com>
2016-08-17 11:39:17 +0100

    Govuk Release-App Postgresql backend
    What
    Govuk is moving away from MySql in favour of Postgres. This relates to https://github.com/alphagov/release/pull/110
    
    How
    Using the govuk_postgresql defined type, I have added the config parameters for the
    release app's database configuration file.

--- modules/govuk/manifests/apps/release/db.pp
+++ modules/govuk/manifests/apps/release/db.pp
@@ -1,11 +1,36 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
+# == Class: govuk::apps::release::db
+#
+# Generates database config for rails application
+#
+# === Parameters:
+#
+# [*postgresql_release_password*]
+#   The password rails will use to authenticate to Postgresql
+#   when runnning in a vagrant environment.
+#
+# [*mysql_release*]
+#   The password rails will use to authenticate to Mysql
+#   when runnning in a vagrant environment.
+#
+# [*backend_ip_range*]
+#   The ip range/LAN the Postgres database is on.
+#
 class govuk::apps::release::db (
+  $postgresql_release_password = '',
   $mysql_release = '',
+  $backend_ip_range   = '10.3.0.0/16',
 ){
 
-  mysql::db {'release_production':
+  mysql::db { 'release_production':
     user     => 'release',
     host     => '%',
     password => $mysql_release,
+  }
+
+  govuk_postgresql::db {'release_production_postgresql':
+    user                    => 'release',
+    password                => $postgresql_release_password,
+    allow_auth_from_backend => true,
+    backend_ip_range        => $backend_ip_range,
   }
 }
